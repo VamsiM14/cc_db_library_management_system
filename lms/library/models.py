ba2a11e77d9f4bbbcfd5775ab397a198885fdb7d
VamsiM14 <40723294+VamsiM14@users.noreply.github.com>
2023-02-13 18:50:09 +0530

    User story 2

--- lms/library/models.py
+++ lms/library/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(default='no.email@given.com')
+    phone = models.CharField(max_length=20, default='+91 98765 43210')
+    faebook_username = models.CharField(max_length=100, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='author_profile_pictures', blank=True, null=True)   
 
     def __str__(self):
         return self.name
--- lms/library/urls.py
+++ lms/library/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
+from .views import BookViewSet, AuthorViewSet
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='book')
+router.register(r'authors', AuthorViewSet, basename='author')
 
 urlpatterns = [
     path('', include(router.urls)),
